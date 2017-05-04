2750149bc98edb3a874b081112abc9a0670e0206
Y4WDMHFW4E1S1W2\Yangyang <526017425@qq.com>
2017-05-04 11:25:28 +0800

    demo1

---
+++ src/main/java/com/wacmx/HelloController.java
@@ -0,0 +1,43 @@
+package com.wacmx;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+/**
+ * Created by Yangyang on 2017/5/2.
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @RequestMapping(value={"hello","hi"},method = RequestMethod.GET)
+    public String hello(){
+        return girlProperties.getCupSize() ;
+    }
+
+    /**
+     * 访问格式:say/100  返回id:100
+     * @param id
+     * @return
+     */
+    @RequestMapping(value={"/say/{id}"},method = RequestMethod.GET)
+    public String say(@PathVariable("id") Integer id){
+        return "id:"+id ;
+    }
+
+    /**
+     * 访问格式:say?id=100  对应
+     * @param id
+     * @return
+     */
+    @RequestMapping(value={"/may"},method = RequestMethod.GET)
+    public String may(@RequestParam("id") Integer id){
+        return "id:"+id ;
+    }
+
+
+}
